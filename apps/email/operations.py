72171e12409ff18686ccbb5a948f749ce72104c9
Jeongseop Lim <jseoplim@gmail.com>
2023-02-18 07:52:07 +0900

    CHG: email

--- apps/email/operations.py
+++ apps/email/operations.py
@@ -1,18 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 
-from apps.chats.models import DailyChat
+from apps.chats.models import DailyChat, ChatRoom
+
+User = get_user_model()
 
 
-class EmailService:
-
-    def send_mail_from_daily_chat(
-        self,
-        daily_chat: DailyChat,
-    ):
-        send_mail(
-            subject=daily_chat.chat_room.name,
-            message=daily_chat.summary_text,
-            from_email='jseoplim2@gmail.com',
-            recipient_list=[daily_chat.chat_room.user.email],
-            fail_silently=False,
-        )
+def send_mail_from_chat_room(
+    chat_room_id: int,
+):
+    chat_room = ChatRoom.objects.get(id=chat_room_id)
+    send_mail(
+        subject=chat_room.name,
+        message=''.join([f'- {daily_chat.summury_text}\n' for daily_chat in chat_room.daily_chats.all()]),
+        from_email='jseoplim2@gmail.com',
+        recipient_list=[chat_room.user.email],
+        fail_silently=False,
+    )
