b1b65ee0ce3192b4665d0cd519197d77bbcac863
Jeongseop Lim <jseoplim@gmail.com>
2023-02-18 04:01:33 +0900

    CHG: 앱 label

--- apps/chats/apps.py
+++ apps/chats/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class ChatConfig(AppConfig):
     name = 'apps.chats'
+    label = 'chats'
     verbose_name = '채팅'
--- apps/users/apps.py
+++ apps/users/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class UserConfig(AppConfig):
     name = 'apps.users'
+    label = 'users'
     verbose_name = '사용자'
