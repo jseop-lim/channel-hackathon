27adc94dacd321731192ac56864fdaac2f2d5ff4
Jeongseop Lim <jseoplim@gmail.com>
2023-02-18 03:46:56 +0900

    Feat: AUTH_USER_MODEL

--- config/settings/base.py
+++ config/settings/base.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'apps.users.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,3 +106,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
