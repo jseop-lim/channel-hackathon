4e5f1267afa21644889d63b60102b7230a7293cc
Jeongseop Lim <jseoplim@gmail.com>
2023-02-18 05:45:33 +0900

    ADD: email operations

---
+++ apps/email/operations.py
@@ -0,0 +1,18 @@
+from django.core.mail import send_mail
+
+from apps.chats.models import DailyChat
+
+
+class EmailService:
+
+    def send_mail_from_daily_chat(
+        self,
+        daily_chat: DailyChat,
+    ):
+        send_mail(
+            subject=daily_chat.chat_room.name,
+            message=daily_chat.summary_text,
+            from_email='jseoplim2@gmail.com',
+            recipient_list=[DailyChat.chat_room.user.email],
+            fail_silently=False,
+        )
